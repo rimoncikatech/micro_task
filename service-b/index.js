3c94c128d05945fe6004ea66476bb969ec3baf6b
admin <rimonroy.cse@gmail.com>
2024-07-16 19:08:26 +0600

    first commit

---
+++ service-b/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = 3001;
+
+const serviceAUrl = process.env.SERVICE_A_URL;
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(serviceAUrl);
+    res.send(`Service B received: ${response.data}`);
+  } catch (error) {
+    res.send('Error connecting to Service A');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Service B listening at http://localhost:${port}`);
+});
