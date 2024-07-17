51ccf7b917ba8b9473e95bfdf89f19d310a8001d
rimoncikatech <99593576+rimoncikatech@users.noreply.github.com>
2024-07-17 12:28:09 +0600

    Update index.js

--- service-a/index.js
+++ service-a/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello From  Micro Service A. Great::!!!!! ');
 });
 
+app.get('/health', (req, res) => {
+    res.json({ status: 'UP' });
+});
+
 app.listen(port, () => {
   console.log(`Service A listening at http://localhost:${port}`);
 });
